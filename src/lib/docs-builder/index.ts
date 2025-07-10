d0b0de3d05e6e0ae38758a3c2352ff70059ecb4c
joeban <joeanddaneil5@gmail.com>
2025-07-10 17:56:12 +0800

    tmp

--- src/lib/docs-builder/index.ts
+++ src/lib/docs-builder/index.ts
@@ -6,7 +6,9 @@ import schema from '$lib/fetcher/schema';
 
 let registry: OpenAPIRegistry | null = null;
 export class DocBuilder {
-	static version: string = '1.0.0';
+	private static openApiVersion: string = '3.1.0';
+	private static apiVersion: string = '1.0.0';
+
 	static get registry() {
 		if (!registry) {
 			registry = new OpenAPIRegistry();
@@ -14,25 +16,82 @@ export class DocBuilder {
 		}
 		return registry;
 	}
-
 	static init() {
 		if (!registry) {
 			registry = new OpenAPIRegistry();
 		}
 	}
 
-	static generatorDoc() {
+	private static formatTags(path: string) {
+		const parts = path.split('/').filter(Boolean);
+		return parts[0] === 'api' ? parts.slice(1) : parts;
+	}
+	static registerPath(_schema: typeof schema) {
+		Object.entries(_schema).forEach(([key, item]) => {
+			const [method, path] = key.split(' ');
+			if (method === 'GET') {
+				DocBuilder.registry.registerPath({
+					method: method.toLowerCase() as 'get' | 'post' | 'put' | 'delete',
+					path: path,
+					summary: path,
+					tags: [this.formatTags(path).join('-')],
+					description: item.description,
+					request: {
+						params: item.request ? item.request : undefined
+					},
+					responses: {
+						200: {
+							description: 'Success response',
+							content: {
+								'application/json': {
+									schema: item.response
+								}
+							}
+						}
+					}
+				});
+			} else {
+				DocBuilder.registry.registerPath({
+					method: method.toLowerCase() as 'get' | 'post' | 'put' | 'delete',
+					path: path,
+					summary: path,
+					tags: [this.formatTags(path).join('-')],
+					description: item.description,
+					request: {
+						body: {
+							content: {
+								'application/json': {
+									schema: item.request
+								}
+							}
+						}
+					},
+					responses: {
+						200: {
+							description: 'Success response',
+							content: {
+								'application/json': {
+									schema: item.response
+								}
+							}
+						}
+					}
+				});
+			}
+		});
+	}
+
+	private static generatorDoc() {
 		if (!registry) {
 			throw new Error('Registry is not initialized');
 		}
 		const generator = new OpenApiGeneratorV3(registry.definitions);
-		const version = '1.0.0';
 
 		const doc = generator.generateDocument({
-			openapi: '3.1.0',
+			openapi: this.openApiVersion,
 			info: {
 				title: 'My API',
-				version: version
+				version: this.apiVersion
 			},
 			servers: [
 				{
@@ -47,81 +106,24 @@ export class DocBuilder {
 		});
 		return doc;
 	}
-
 	static writeDocToFile() {
 		let doc = null;
-		doc = DocBuilder.generatorDoc();
+		doc = this.generatorDoc();
 		if (!doc) {
 			throw new Error('Failed to generate OpenAPI document');
 		}
 
-		const destDir = path.join('static', 'docs', DocBuilder.version);
+		const destDir = path.join('static', 'docs', this.apiVersion);
 		if (!fs.existsSync(destDir)) {
 			fs.mkdirSync(destDir, { recursive: true });
 		}
 		fs.writeFileSync(path.join(destDir, 'swagger.json'), JSON.stringify(doc, null, 2));
 	}
-	// 這裡可以添加其他方法來處理文檔生成邏輯
 }
 export async function docsBuilder() {
-	function formatTags(path: string) {
-		const parts = path.split('/').filter(Boolean);
-		return parts[0] === 'api' ? parts.slice(1) : parts;
-	}
-	DocBuilder.init();
+	await DocBuilder.init();
 
-	Object.entries(schema).forEach(([key, item]) => {
-		const [method, path] = key.split(' ');
-		if (method === 'GET') {
-			DocBuilder.registry.registerPath({
-				method: method.toLowerCase() as 'get' | 'post' | 'put' | 'delete',
-				path: path,
-				summary: path,
-				tags: [formatTags(path).join('-')],
-				description: item.description,
-				request: {
-					params: item.request ? item.request : undefined
-				},
-				responses: {
-					200: {
-						description: 'Success response',
-						content: {
-							'application/json': {
-								schema: item.response
-							}
-						}
-					}
-				}
-			});
-		} else {
-			DocBuilder.registry.registerPath({
-				method: method.toLowerCase() as 'get' | 'post' | 'put' | 'delete',
-				path: path,
-				summary: path,
-				tags: [formatTags(path).join('-')],
-				description: item.description,
-				request: {
-					body: {
-						content: {
-							'application/json': {
-								schema: item.request
-							}
-						}
-					}
-				},
-				responses: {
-					200: {
-						description: 'Success response',
-						content: {
-							'application/json': {
-								schema: item.response
-							}
-						}
-					}
-				}
-			});
-		}
-	});
+	await DocBuilder.registerPath(schema);
 
-	DocBuilder.writeDocToFile();
+	await DocBuilder.writeDocToFile();
 }
